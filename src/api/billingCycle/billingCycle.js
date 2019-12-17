4a1ee2b69ec6325b212e233d80ba801c1122c3bc
Domenico <domedfd@gmail.com>
2019-12-17 17:05:57 -0300

    Cambiado ablicado y codio y fecha

--- src/api/billingCycle/billingCycle.js
+++ src/api/billingCycle/billingCycle.js
@@ -2,16 +2,21 @@ const restful = require("node-restful");
 const mongoose = restful.mongoose;
 
 const creditSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  value: { type: Number, min: 0, require: true }
+  name: { type: String },
+  value: {
+    type: Number,
+    min: 0
+    // , require: true
+  }
 });
 
 const debtSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String },
   value: {
     type: Number,
-    min: 0,
-    required: [true, "Informe o valor do debito!"]
+    min: 0
+    // ,
+    // required: [true, "Informe o valor do debito!"]
   },
   status: {
     type: String,
@@ -23,8 +28,8 @@ const debtSchema = new mongoose.Schema({
 
 const billingCycleSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  month: { type: Number, min: 1, max: 12, required: true },
-  year: { type: Number, min: 1970, max: 2100, required: true },
+  code: { type: String },
+  date: { type: String, required: true },
   credits: [creditSchema],
   debts: [debtSchema]
 });
