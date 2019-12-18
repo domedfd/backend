703050649f26da35cb1aa0ef6be331abe0dca3c7
Domenico <domedfd@gmail.com>
2019-12-18 11:12:44 -0300

    enumeracio

--- src/api/user/user.js
+++ src/api/user/user.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: false,
     uppercase: true,
-    enum: ["ADMINISTRADOR", "USER"]
+    enum: ["ADMINISTRADOR", "USUARIO"]
   }
 });
 
