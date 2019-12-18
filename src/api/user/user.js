d36c8fc2d120d50a95408ca08363b2a43e6c0fb9
Domenico <domedfd@gmail.com>
2019-12-18 10:39:20 -0300

    adcionado perfil en schema usuario

--- src/api/user/user.js
+++ src/api/user/user.js
@@ -4,7 +4,13 @@ const mongoose = restful.mongoose;
 const userSchema = new mongoose.Schema({
   name: { type: String, require: true },
   email: { type: String, require: true },
-  password: { type: String, min: 6, max: 12, require: true }
+  password: { type: String, min: 6, max: 12, require: true },
+  perfil: {
+    type: String,
+    required: false,
+    uppercase: true,
+    enum: ["ADMINISTRADOR", "USER"]
+  }
 });
 
 module.exports = restful.model("User", userSchema);
